81e1f43ef761ba293ffb5f93902dc0f3bb37c207
unknown <jasmineakbari@yahoo.com>
2021-03-07 11:08:01 -0500

    completed thought and reaction schemas added reaction virtual

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: true,
+        validate: {
+          validator: function(v) {
+              return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+          },
+          message: "Please enter a valid email"
+        },
+      required: [true, "Email required"]
+    },
+    thought: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+      ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+      ]
+    },
+    {
+      toJSON: {
+          virtuals: true
+      }
+    }
+);
+
+// virtual to track friendCount
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
