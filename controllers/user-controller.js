779ee13fdd6f9faca91fb21703fda421b1d81239
unknown <jasmineakbari@yahoo.com>
2021-03-07 11:23:19 -0500

    added create find and update user by id routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,7 +1,7 @@
 const { User } = require('../models');
 
 const userController = {
-    getAllPizza(req, res) {
+    getAllUser(req, res) {
         User.find({})
         .populate({
           path: 'thought',
@@ -14,6 +14,45 @@ const userController = {
             console.log(err);
             res.status(400).json(err);
           });
+    },
+
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .populate({
+          path: 'thought',
+          select: '-__v'
+        })
+          .select('-__v')
+          .then(dbUserData => {
+            if (!dbUserData) {
+              res.status(404).json({ message: 'No User found with this id!' });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+          });
+    },
+
+    createUser({ body }, res) {
+        User.create(body)
+          .then(dbUserData => res.json(dbUserData))
+          .catch(err => res.status(400).json(err));
+    },
+
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, 
+          runValidators: true })
+          .then(dbUserData => {
+            if (!dbUserData) {
+              res.status(404).json({ message: 'No User found with this id!' });
+              return;
+            }
+            res.json(dbUserData);
+          })
+          .catch(err => res.status(400).json(err));
     },
 
     deleteUser({ params }, res) {
